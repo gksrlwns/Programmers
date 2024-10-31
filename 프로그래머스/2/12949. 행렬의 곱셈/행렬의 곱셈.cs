63ce314a5da0b306bb0997b9f48839e0b3949207
kijunHan <gksrlwns96@naver.com>
2024-10-31 17:51:07 +0900

    [level 2] Title: 행렬의 곱셈, Time: 26.67 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cs
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cs
@@ -5,18 +5,18 @@ public class Solution {
         int row = arr1.GetLength(0);
         int col = arr2.GetLength(1);
         int[,] answer = new int[row,col];
-        for (int i = 0; i < row; i++)
+        
+        for(int i = 0; i< row; i++)
         {
-            for (int j = 0; j < col; j++)
+            for(int j = 0; j < col; j++)
             {
-                for (int k = 0; k < arr1.GetLength(1); k++)
+                for(int k = 0; k < arr1.GetLength(1); k++)
                 {
-                    //Console.Write($"[{i},{j}] =  {arr1[i, k]} * {arr2[k, j]}");
-                    answer[i, j] += arr1[i,k] * arr2[k,j];
+                    answer[i,j] += arr1[i,k] * arr2[k,j];
                 }
-                //Console.WriteLine();
             }
         }
         return answer;
+        
     }
 }
